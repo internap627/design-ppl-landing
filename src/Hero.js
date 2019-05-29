6edb0b91e23f96368fefbda91314f646223fe11c
Nkosi Phillip <internap627@gmail.com>
2019-05-29 15:52:12 +0100

    nav & hero markup

---
+++ src/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => (
+    <div className='hero'>
+        <h1>At the front line of Digital Systems for Web & Mobile</h1>
+    </div>
+)
+
+export default Hero
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => (
+    <div className='nav'>
+        <h1>DESIGNPPL</h1>
+
+    </div>
+)
+
+export default Nav
