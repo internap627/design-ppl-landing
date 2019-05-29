071501d3682c4a2be3117b090923a14237a1e9dd
Nkosi Phillip <internap627@gmail.com>
2019-05-30 00:16:27 +0100

    welcome section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Nav from './Nav'
 import Hero from './Hero'
+import Welcome from './Welcome'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
+      <Welcome />
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Welcome.css'
+
+const Welcome = () => (
+    <div className='welcome'>
+        <p>WELCOME TO DESIGNPPL</p>
+        <h1>Why us</h1>
+        <h2>Not just Designers, Developers, UX / UI Experts & Project Managers <br/> But a force instead. </h2>
+        <h2>A collection of great minds, doing even greater things... together ... for companies that want to lead.</h2>
+
+    </div>
+)
+
+export default Welcome
